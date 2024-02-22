721043e1dacfc59d0ba20fb89542640bc7fc2923
bbqw_ng <bbqw_ng>
2024-02-22 00:13:34 -0500

    added some keymaps for more convenient time with vim.

--- nvim/lua/scripts/remap.lua
+++ nvim/lua/scripts/remap.lua
@@ -1,3 +1,12 @@
 --[["n" means normal mode, im sure you can guess what the other modes are :)]]--
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--[[Easy copying to system clipboard]]--
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+--[[This makes it so your delete buffer wont change after deleting something.]]--
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>D", "\"_d")
