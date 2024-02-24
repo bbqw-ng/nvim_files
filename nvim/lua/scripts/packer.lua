bbe26c40af5302ad0210fdcd97ab53e5d61fad3c
bbqw_ng <brwng902@gmail.com>
2024-02-24 15:17:40 -0500

    disabled nvimtree

--- nvim/lua/scripts/packer.lua
+++ nvim/lua/scripts/packer.lua
@@ -34,11 +34,12 @@ return require("packer").startup(function(use)
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
-  use {
+  --This is the tree you can turn it on if you want.
+  --[[use {
   'nvim-tree/nvim-tree.lua',
   requires = {
     'nvim-tree/nvim-web-devicons', -- optional
   },
-}
+} ]]--
 
 end)
