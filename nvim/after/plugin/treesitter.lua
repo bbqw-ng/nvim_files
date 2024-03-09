aeb854eb70d51856b611dfb1bf36a07a72501601
bbqw_ng <brwng902@gmail.com>
2024-03-08 22:38:35 -0500

    compiz directory is for OPENGL 'francis added' added mouse mode on to tmux config

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -21,4 +21,9 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+  --allows for html autoclose <div>
+  autotag = {
+    enable = true,
+  }
 }
